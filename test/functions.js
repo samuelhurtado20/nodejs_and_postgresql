c1b6113ba22a3d8293bfd3313de5e656d54f8693
samuelhurtado20 <samuelhurtado20@hotmail.com>
2022-04-29 23:55:01 -0500

    example asdrubal

---
+++ test/functions.js
@@ -0,0 +1,14 @@
+function Sumar(num1, num2) {
+  //declaro la variable suma y le asigno el valor de la suma
+  var suma = num1 + num2
+  //imprimo por consola
+  console.log('la suma es ' + suma)
+  //devuelvo el resultado
+  return suma
+}
+
+//declaro la variable resultado y le asigno el valor que retorna la funcion sumar
+var resultado = Sumar(7, 3)
+//imprimo por consola el valor de la variable resultado
+console.log(resultado)
+ß
